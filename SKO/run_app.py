dc159f9b6d882fa923d0ed07b3497dfab709155c
Kaze Wong <kazewong.physics@gmail.com>
2024-12-08 11:03:28 -0500

    port changes

--- SKO/run_app.py
+++ SKO/run_app.py
@@ -1,39 +1,41 @@
-# import os
+import os
 
-# from flask import Flask
-# from flask_cors import CORS
+from flask import Flask
+from flask_cors import CORS
 
 
-# def create_app(test_config=None):
-#     # create and configure the app
-#     app = Flask(__name__, instance_relative_config=True)
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
 
-#     import database, webhook
-#     app.register_blueprint(database.database_service, url_prefix="/database")
-#     app.register_blueprint(webhook.webhook_service, url_prefix="/webhook")
+    from . import database, webhook, minio
+    app.register_blueprint(database.database_service, url_prefix="/flask/database")
+    app.register_blueprint(webhook.webhook_service, url_prefix="/flask/webhook")
+    app.register_blueprint(minio.minio_service, url_prefix="/flask/minio")
 
-#     if test_config is None:
-#         # load the instance config, if it exists, when not testing
-#         app.config.from_pyfile('config.py', silent=True)
-#         app.config['SECRET_KEY']
-#     else:
-#         # load the test config if passed in
-#         app.config.from_mapping(test_config)
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+        app.config['SECRET_KEY']
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-#     # ensure the instance folder exists
-#     try:
-#         os.makedirs(app.instance_path)
-#     except OSError:
-#         pass
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-#     # a simple page that says hello
-#     @app.route('/hello')
-#     def hello():
-#         return 'Hello, World!'
+    @app.route('/flask/hello')
+    def hello():
+        return 'Hello, World!'
 
-#     return app
+    CORS(app)
 
-# if __name__ == "__main__":
-#     app = create_app()
-#     CORS(app)
-#     app.run(host="0.0.0.0", port=8001)
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    CORS(app)
+    app.run(host="0.0.0.0", port=9500)
